80a3a3baa2be38b8a839a773a41c35dfc17c588c
MitsuSDK <babouthiam4@gmail.com>
2026-02-10 23:04:12 +0100

    speech to text funcion

--- folder/AiAssistant.py
+++ folder/AiAssistant.py
@@ -2,27 +2,35 @@
 
 import speech_recognition as sr
 
-recognizer = sr.Recognizer()
-recognizer.pause_threshold = 2.0  # wait 2 seconds of silence
 
 
-''' recording the sound '''
+def speech_to_text():
 
-with sr.Microphone() as source:
-    print("Adjusting noise ")
-    recognizer.adjust_for_ambient_noise(source, duration=1)
-    print("Recording for 4 seconds")
-    recorded_audio = recognizer.listen(source, timeout=4)
-    print("Done recording")
+    recognizer = sr.Recognizer()
+    recognizer.pause_threshold = 2.0  # wait 2 seconds of silence
 
-''' Recorgnizing the Audio '''
-try:
-    print("Recognizing the text")
-    text = recognizer.recognize_google(
-            recorded_audio, 
-            language="en-US"
-        )
-    print("Decoded Text : {}".format(text))
+    ''' recording the sound '''
 
-except Exception as ex:
-    print(ex)
+    with sr.Microphone() as source:
+        print("Adjusting noise ")
+        recognizer.adjust_for_ambient_noise(source, duration=1)
+        print("Recording for 4 seconds")
+        recorded_audio = recognizer.listen(source, timeout=4)
+        print("Done recording")
+
+    ''' Recorgnizing the Audio '''
+    try:
+        print("Recognizing the text")
+        text = recognizer.recognize_google(
+                recorded_audio, 
+                language="en-US"
+            )
+        #print("Decoded Text : {}".format(text))
+
+        return text
+    
+    except Exception as ex:
+        print(ex)
+
+output_of_speech = speech_to_text()
+print("This is what I said: ", output_of_speech)
